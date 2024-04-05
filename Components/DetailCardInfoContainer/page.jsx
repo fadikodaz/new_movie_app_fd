95ce47a7d72fbf59db454ff4eb313651ef2c1e46
Fahad <121719882+fadikodaz@users.noreply.github.com>
2024-04-06 00:31:20 +0500

    Add files via upload

--- Components/DetailCardInfoContainer/page.jsx
+++ Components/DetailCardInfoContainer/page.jsx
@@ -1,45 +1,101 @@
-import React from 'react'
-import Link from 'next/link'
+'use clinet'
+import React, { useEffect, useState, useRef } from 'react';
 import './DetailInfoContainer.css'
+import DetailCardLoader from '../DetailCardLoader/page'
 
 const DetailInfoContainer = ({result}) => {
+    
+    const [isLoading, setIsLoading] = useState(true);
+    console.log(result)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
   return (
+    <>
+    {
+    isLoading 
+     ? 
+    (
+        <DetailCardLoader />
+    ) 
+     :
+    
     <div className='infoWrapper'>
 
-     <div className="Watch_add_Btns_Wrapper">
-          <a className='watchBtn' target='_blank' href={result?.homepage}>
-              <i className="ri-play-fill"></i>
-              Watch now
-          </a>
-          <button className='addBtn'>
-              <i className="ri-add-line"></i>
-              Add to favorite
-          </button>
-      </div>
-      
-      <div className="MovieTitleWrapper">
-          <a className='title' target='_blank' href={result?.homepage}>{result?.title || result?.name}</a>
-          <p className="tagLine">{result?.tagline}</p>
-      </div>
-      
-      <div className="videoType_Btns_Wrapper">
-          <button className='trailerBtn'>
-              <i className="ri-vidicon-fill"></i> 
-              Trailer
-          </button>
-          <button className='hdBtn'>
-              HD
-          </button>
-          <button className='imdbBtn'>
-              IMDB
-          </button>
-      </div>
-      
-      <div className="Movie_Over_View_Wrapper">
-          <p>{result?.overview}</p>
-      </div>
+        <div className="Watch_add_Btns">
+            <a className='watchBtn' target='_blank' href={result?.homepage}>
+                <i className="ri-play-fill"></i>
+                Watch now
+            </a>
+            <button className='addBtn'>
+                <i className="ri-add-line"></i>
+                Add to favorite
+            </button>
+        </div>
+        
+        <div className="MovieTitle">
+            <a className='title' target='_blank' href={result?.homepage}>{result?.title || result?.name}</a>
+            <p className="tagLine">{result?.tagline}</p>
+        </div>
+        
+        <div className="videoType_Btns">
+            <button className='trailerBtn'>
+                <i className="ri-vidicon-fill"></i> 
+                Trailer
+            </button>
+            <button className='hdBtn'>
+                HD
+            </button>
+            <button className='imdbBtn'>
+                IMDB
+            </button>
+        </div>
+        
+        <div className="Movie-Over_View">
+            <p>{result?.overview}</p>
+        </div>
+
+        <div className="MovieDetails">
+                <div className="Column">
+                    <div className="MovieDetailsSection">
+                        <h4>Released Date:</h4>
+                        <p>{result?.release_date}</p>
+                    </div>
+                    <div className="MovieDetailsSection">
+                        <h4>Genre:</h4>
+                        <div className="Genres">
+                            {result?.genres.map(gen => (
+                                <p key={gen.id}>{  ',' + gen.name }</p>
+                                ))}
+                        </div>
+                    </div>
+                </div>
+                <div className="Column">
+                    <div className="MovieDetailsSection">
+                        <h4>Duration:</h4>
+                        <p>{result?.runtime} min</p>
+                    </div>
+                    <div className="MovieDetailsSection">
+                        <h4>Production:</h4>
+                        <div className="ProductionCompanies">
+                            {result?.production_countries.map(pr => (
+                                <p key={pr.id}>{pr.name}</p>
+                            ))} 
+                        </div>
+                    </div>
+                </div>
+        </div>
 
     </div>
+
+    }
+    </>
   )
 }
 
--- Components/MoviesCards/page.jsx
+++ Components/MoviesCards/page.jsx
@@ -1,75 +1,95 @@
 'use client'
-import React, { useEffect, useState, useRef } from 'react'
-import { useRouter } from 'next/navigation'
-import '@/style/MovieCards.css'
-import SkeletonLoader from '@/Components/CardsSkeletonLoader/page'
-import img from '@/public/Assets/no-poster.png'
+import React, { useEffect, useState, useRef } from 'react';
+import { useRouter } from 'next/navigation';
+import '@/style/MovieCards.css';
+import SkeletonLoader from '@/Components/CardsSkeletonLoader/page';
+import img from '@/public/Assets/no-poster.png';
 
-
-
-const MoviesCards = ({allData}) => {
-    
+const MoviesCards = ({ allData }) => {
     const imgRef = useRef(null);
-    const router = useRouter()
+    const router = useRouter();
     const [isLoading, setIsLoading] = useState(true);
-    const Image_Base_Url = 'https://image.tmdb.org/t/p/original/'
+    const [cachedImageURLs, setCachedImageURLs] = useState({});
+    const Image_Base_Url = 'https://image.tmdb.org/t/p/original/';
+
+    useEffect(() => {
+        // Fetch cached image URLs from local storage
+        const cachedData = localStorage.getItem('cachedImageURLs');
+        if (cachedData) {
+            setCachedImageURLs(JSON.parse(cachedData));
+        }
+    }, []);
+
+    useEffect(() => {
+        // Cache image URLs
+        const fetchAndCacheImageURLs = async () => {
+            const urlsToCache = {};
+            for (const detail of allData) {
+                const imageURL = Image_Base_Url + detail?.poster_path;
+                urlsToCache[detail.id] = imageURL;
+            }
+            setCachedImageURLs(urlsToCache);
+            localStorage.setItem('cachedImageURLs', JSON.stringify(urlsToCache));
+        };
+
+        fetchAndCacheImageURLs();
+    }, [allData]);
 
     useEffect(() => {
         const timer = setTimeout(() => {
-        setIsLoading(false);
+            setIsLoading(false);
         }, 1000);
-    
-        return () => clearTimeout(timer); 
+
+        return () => clearTimeout(timer);
     }, []);
-    
-    const  MediaDetailHandler = (type,name,id) => {
-        router.push(`/${type}/${name}?id=${id}`)
-    }
 
-    const cards =  allData.map((detail)=>{
+    const MediaDetailHandler = (type, name, id) => {
+        router.push(`/${type}/${name}?id=${id}`);
+    };
 
-        let type  = detail?.media_type
-        let title = detail?.title || detail?.name
+    const cards = allData.map((detail) => {
+        let type = detail?.media_type;
+        let title = detail?.title || detail?.name;
         let cleanTitle = title.replace(/[ :]+/g, '-');
-        let id    = detail?.id
+        let id = detail?.id;
 
-    return (
-        
-        isLoading
-        ?
-        <SkeletonLoader/>
-        :
-        <div className='CardBox' key={detail?.id}>
-            <div className="imgBox" onClick={()=>{MediaDetailHandler(type, cleanTitle, id)}}>
-                <img
-                    ref={imgRef}
-                    src={Image_Base_Url+detail?.poster_path}
-                    alt={title} 
-                />
-                <div className='PlayBtn'>
-                    <i className="ri-play-fill"></i>
+        return (
+            <div className='CardBox' key={detail?.id}>
+                {isLoading ? (
+                    <SkeletonLoader />
+                ) : (
+                    <div className="imgBox" onClick={() => { MediaDetailHandler(type, cleanTitle, id) }}>
+                        <img
+                            ref={imgRef}
+                            src={cachedImageURLs[id] || img}
+                            alt={title}
+                            loading="lazy" // lazy loading
+                        />
+                        <div className='PlayBtn'>
+                            <i className="ri-play-fill"></i>
+                        </div>
+                    </div>
+                )}
+                <h2 className='title' title={title}>
+                    {detail?.title || detail?.name}
+                </h2>
+                <div className="Date_Type">
+                    <span className='date'>
+                        {detail?.release_date}
+                    </span>
+                    <span className='type'>
+                        {detail?.media_type}
+                    </span>
                 </div>
             </div>
-            <h2 className='title' title={title}>
-                {detail?.title || detail?.name}
-            </h2>
-            <div className="Date_Type">
-                <span className='date'>
-                    {detail?.release_date}
-                </span>
-                <span className='type'>
-                    {detail?.media_type}
-                </span>
-            </div>
-        </div>
-        )
-    })
+        );
+    });
 
     return (
         <div className='CardsGridbox'>
             {cards}
         </div>
-    )
-}
+    );
+};
 
-export default MoviesCards
+export default MoviesCards;
