3b4faeb19ffbbf0069b7fc7e9983cb70d12d09f5
Muhammad Fahad Nayyer <121719882+fadikodaz@users.noreply.github.com>
2025-03-04 11:01:37 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'image.tmdb.org',
-          port: '',
-          pathname: '/t/p/original/**',
-        },
-      ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/original/**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL", // Allows iframe embedding
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' *;", // Allows all sites to embed
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
